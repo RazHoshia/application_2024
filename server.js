0ba8717fbfa72fbc30abeab48925c1c8c6c09475
Raz Hoshia <razhoshia@gmail.com>
2024-09-14 17:27:04 +0300

    add login + signup

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const bodyParser = require('body-parser');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const shopRoutes = require('./routes/shopRoutes'); // Updated to use shop routes
+const userRoutes = require('./routes/userRoutes');
+const shopRoutes = require('./routes/shopRoutes'); // Add shop routes
+const userAuthRoutes = require('./routes/userAuthRoutes'); // Add user auth routes
 
 // Middleware to parse request bodies
 app.use(bodyParser.json());
@@ -13,8 +15,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Serve static files
 app.use(express.static('public'));
 
-// Use Shop routes
-app.use('/shops', shopRoutes); // Updated to use shop routes
+// Use routes
+app.use('/users', userRoutes);
+app.use('/shops', shopRoutes); // Include shop routes
+app.use('/auth', userAuthRoutes); // Include authentication routes
 
 // Start the server
 app.listen(PORT, () => {
