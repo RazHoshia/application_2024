519c76adcbc5db61ab8580e0a2eedd36a6a5e84d
Raz Hoshia <razhoshia@gmail.com>
2024-09-14 17:31:35 +0300

    add cart manager

--- server.js
+++ server.js
@@ -5,20 +5,27 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 const userRoutes = require('./routes/userRoutes');
-const shopRoutes = require('./routes/shopRoutes'); // Add shop routes
-const userAuthRoutes = require('./routes/userAuthRoutes'); // Add user auth routes
+const shopRoutes = require('./routes/shopRoutes');
+const userAuthRoutes = require('./routes/userAuthRoutes');
+const cartManagerRoutes = require('./routes/cartManagerRoutes');
 
 // Middleware to parse request bodies
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Serve static files
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Use routes
 app.use('/users', userRoutes);
-app.use('/shops', shopRoutes); // Include shop routes
-app.use('/auth', userAuthRoutes); // Include authentication routes
+app.use('/shops', shopRoutes);
+app.use('/auth', userAuthRoutes);
+app.use('/cart', cartManagerRoutes);
+
+// Serve the main HTML file
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 // Start the server
 app.listen(PORT, () => {
